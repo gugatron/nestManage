01c6c6577caefdd2e5c2de56d8d9c82f99d4896e
gugatron <golubovic.golub@hotmail.com>
2025-02-14 19:49:13 +0000

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+//Student data
+int pigeonID = 15;
+int pigeonAge = 23;
+float nestFee = 75.25;
+char pigeonNest = 'B';
+
+//Print variables
+cout << "Pigeon ID:" << pigeonID << "\n";
+cout << "Pigeon Avrage age in Nest:" << pigeonAge << "\n";
+cout << "Nest Fee:" << nestFee << "\n";
+cout << "Pigeon Nest:" << pigeonNest << "\n";
+
+}
